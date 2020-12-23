71fcdea8fab5b3a7a235bf64dd207a9573c7521e
Anzette <anzette@linumlabs.com>
2020-12-23 13:42:53 +0200

    Add boilerplate files

---
+++ packages/react-app/src/components/boilerplate/boilerplate.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./boilerplateStyles";
+import { withStyles } from "@material-ui/core/styles";
+
+function Boilerplate(props) {
+  return <></>;
+}
+
+export default withStyles(styles)(Boilerplate);
