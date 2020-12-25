7aa61bff292715ec200122d26d24e14f7c3e8b80
William <william@linumlabs.com>
2020-12-25 09:36:13 +0200

    very hacky, but frontend compiles

--- packages/react-app/src/SellFlow/0-CreateNFT/createNFT.jsx
+++ packages/react-app/src/SellFlow/0-CreateNFT/createNFT.jsx
@@ -1,11 +1,12 @@
-import React, { useContext} from 'react';
-import {Store} from "../../store/store";
+import React, { useContext, useState } from 'react';
+// import {Store} from "../../store/store";
 import styles from "./createNFTStyles";
 import {withStyles} from "@material-ui/core/styles";
 
 
 function CreateNFT(props) {
-    const {state, actions} = useContext(Store);
+    // const {state, actions} = useContext(Store);
+    const [ state, setState ] = useState({});
     const {classes, navigation} = props;
     const {next, go} = navigation;
 
@@ -22,10 +23,31 @@ function CreateNFT(props) {
     //     const tokenName = `${nftName.toUpperCase()}SHARE-01`;
     //     const tokenSymbol = tokenName.slice(0, 4);
     // }
+
+    const setNFTName = async e => {
+
+    }
+
+    const setTokenName = async e => {
+
+    }
+
+    const setTokenSymbol = async e => {
+
+    }
+
+    const setNFTSymbol = async e => {
+
+    }
+
+    const setNFTUrl = async e => {
+        
+    }
+
     const handleNameChange =(e)=> {
-        actions.setNFTName(e.target.value)
-        actions.setTokenName(e.target.value)
-        actions.setTokenSymbol(e.target.value)
+        setNFTName(e.target.value)
+        setTokenName(e.target.value)
+        setTokenSymbol(e.target.value)
     }
     const {name, symbol, url} = state.nftDetails;
 
@@ -42,10 +64,10 @@ function CreateNFT(props) {
                 <input onChange={handleNameChange} value={name} className={classes.input} type="text" name="name" placeholder="example: MyCoolNFT"/>
 
                 <label className={classes.label} for="symbol">Symbol for NFT</label>
-                <input onChange={(e)=> actions.setNFTSymbol(e.target.value)} value={symbol} className={classes.input} type="text" name="symbol" placeholder="MYCT"/>
+                <input onChange={(e)=> setNFTSymbol(e.target.value)} value={symbol} className={classes.input} type="text" name="symbol" placeholder="MYCT"/>
 
                 <label  className={classes.label}  for="url">URL</label>
-                <input onChange={(e)=> actions.setNFTUrl(e.target.value)} value={url} className={classes.input} type="text" name="url" placeholder="URL"/>
+                <input onChange={(e)=> setNFTUrl(e.target.value)} value={url} className={classes.input} type="text" name="url" placeholder="URL"/>
 
                 <div className={classes.btnBar}>
                     <button onClick={handlePrevious} className={classes.btnLeft}  >Back</button>
