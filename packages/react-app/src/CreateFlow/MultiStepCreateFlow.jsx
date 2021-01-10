f0f46025ba41dbb71ce4cb0173d52bf1efbf12d1
William <william@linumlabs.com>
2021-01-10 10:51:52 +0200

    fixing create flow

---
+++ packages/react-app/src/CreateFlow/MultiStepCreateFlow.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { useStep } from "react-hooks-helper";
+import { Transition } from "react-transition-group";
+// import LandingPage from "../components/landingPage/landingPage";
+import Overview from "./0-Overview/Overview";
+import CreateNFT from "./1-CreateNFT/createNFT";
+import ChooseCurve from "./3-ChooseCurve/chooseCurve";
+import SelectPrice from "./2-SelectPrice/selectPrice";
+import Review from "./4-Review/review";
+import Final from "./5-Final/final";
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+};
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 1 },
+  exited: { opacity: 0 },
+};
+
+const steps = [
+  { id: "overview"},
+  { id: "createNFT"},
+  { id: "selectPrice" },
+  { id: "chooseCurve" },
+  { id: "review" },
+  { id: "final" },
+];
+
+const MultiStepCreateFlow = () => {
+  const { step, navigation } = useStep({ initialStep: 0, steps });
+  const { id } = step;
+
+  
+  return (
+    <>
+      <Transition
+        in={id === "overview"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <Overview navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+      <Transition
+        in={id === "createNFT"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <CreateNFT navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+      <Transition
+        in={id === "selectPrice"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <SelectPrice navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+      
+      <Transition
+        in={id === "chooseCurve"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <ChooseCurve navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+      <Transition
+        in={id === "review"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <Review navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+      <Transition
+        in={id === "final"}
+        timeout={duration}
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <Final navigation={navigation} />
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+
+};
+
+export default MultiStepCreateFlow;
